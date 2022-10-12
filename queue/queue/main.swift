5f8e037a96e5bfcb60f4c1d91b1f130ff48b5440
kickbell <dev.kickbell@gmail.com>
2022-10-12 17:02:13 +0900

    add queue

--- queue/queue/main.swift
+++ queue/queue/main.swift
@@ -7,5 +7,33 @@
 
 import Foundation
 
-print("Hello, World!")
+
+//Queue()
+
+var queue: [Int] = []
+
+func enqueue(_ data: Int) {
+    queue.append(data)
+}
+
+func dequeue() -> Int {
+    if let _ = queue.first {
+        return queue.removeFirst()
+    }
+    return -1
+}
+
+(1...10).forEach {
+    enqueue($0)
+}
+
+print(queue)
+print(dequeue())
+print(dequeue())
+print(dequeue())
+
+
+//LifoQueue()
+
+//PriorityQueue()
 
