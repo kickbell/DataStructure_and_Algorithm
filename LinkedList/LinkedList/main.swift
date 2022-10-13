97bd29edaedb1fc9fc8b33089fe1906822ccb578
kickbell <dev.kickbell@gmail.com>
2022-10-13 13:06:22 +0900

    Add insert 3 and 4 data

--- LinkedList/LinkedList/main.swift
+++ LinkedList/LinkedList/main.swift
@@ -56,10 +56,18 @@ print(node?.data ?? 0)
 
 //중간에 데이터 추가하기
 
+//1. 추가할 데이터 생성, 헤드값 초기화. 맨처음부터 다시 돌아야 되니까
 var node3 = Node(data: 3.5)
 node = head
+
+//2. 어디에 넣을지 찾는 변수 추가
 var search = true
 
+//3. 어디에 넣을지 찾기.
+// 그 이전까지는 주소값을 넣어주다가 3을 찾으면 종료시켜버림.
+// 중요한 것은 1 + 주소값, 2 + 주소값, 3 -> 종료이기 때문에
+// 4,5,6,7,8.. 같은애들애는 주소값이 없어. 즉, 미리 데이터가 다 들어가있는 상황에서
+// 중간에 데이터를 삽입할때 쓰는 방법이라는 말임.
 while search {
     if node?.data == 3.0 {
         search = false
@@ -68,9 +76,13 @@ while search {
     }
 }
 
-let nonext = node?.next
-node?.next = node3
-node3.next = nonext
+//4. 새로 넣을 데이터에 주소값 연결하기
+// 자, 보면
+// 지금 node.next는 원래 3->4 를 가리키던 주소값임.
+// 그래서 일단 그걸 다른 변수에 담아놓는다.
+let lastNext = node?.next
+node?.next = node3 // 그리고 가리키는 값을 node3이 가리키도록 바꿔.
+node3.next = lastNext // 그리고 마지막으로 node3이 4(lastNext)를 가리키도록 바꿔준다.
 
 
 print("==================================================================")
