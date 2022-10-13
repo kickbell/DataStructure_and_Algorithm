04cc1b91ecb571fec6a31ac2adb9459e497ac7bb
kickbell <dev.kickbell@gmail.com>
2022-10-13 17:08:20 +0900

    add linkedliist delete

--- LinkedList2/LinkedList2/main.swift
+++ LinkedList2/LinkedList2/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Node<T> {
+class Node<T: Equatable> {
     var data: T
     var pointer: Node<T>?
     
@@ -18,21 +18,37 @@ class Node<T> {
 }
 
 class NodeManager<T> {
-    var head: Node<T>
-    var node: Node<T>?
+    var head: Node<Int>?
+    var node: Node<Int>?
     
-    init(data: T) {
+    init(data: Int) {
         self.head = Node(data: data)
     }
     
-    func add(_ data: T) {
-        node = head //처음 노드 할당
+    func add(_ data: Int) {
+        node = self.head //처음 노드 할당
         
         while node?.pointer != nil { //마지막 노드를 찾을때까지 반복
             node = node?.pointer
         }
         
         node?.pointer = Node(data: data) //마지막 노드찾음 ㅇㅋ
+    }
+    
+    //특정 데이터 삭제하기
+    func delete(_ data: Int) {
+        if self.head!.data == data { //1. 맨앞에 있는 애를 삭제하는 경우 (헤드를 삭제하는 경우)
+            self.head = self.head!.pointer
+        } else { //2. 마지막에 있는 애를 삭제하는 경우, 3. 중간에 있는 애를 삭제하는 경우
+            node = self.head
+            while node?.pointer != nil {
+                if node!.pointer!.data == data {
+                    node?.pointer = node?.pointer?.pointer
+                } else {
+                    node = node?.pointer
+                }
+            }
+        }
     }
     
     //단순 전체 데이터 출력 용도 메소드
@@ -47,15 +63,26 @@ class NodeManager<T> {
     }
 }
 
+let linkedList = NodeManager<Int>(data: 0)
 
-let linkedList = NodeManager(data: 0)
+print("데이터 추가 후 출력======================================================")
 
-(10...15).forEach {
-    linkedList.add($0)
+for data in 1...10 {
+    linkedList.add(data)
 }
+linkedList.desc()
+
+print("4 삭제 확인======================================================")
+
+linkedList.delete(4)
 
 linkedList.desc()
 
+print("10 삭제 확인======================================================")
+
+linkedList.delete(10)
+
+linkedList.desc()
 
 
 
