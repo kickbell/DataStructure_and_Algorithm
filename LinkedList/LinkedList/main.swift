57bd4d0072d751bd169da17971653b7c181595de
kickbell <dev.kickbell@gmail.com>
2022-10-13 12:53:41 +0900

    linked list 2

--- LinkedList/LinkedList/main.swift
+++ LinkedList/LinkedList/main.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 
+//왜 구조체로 안하고 클래스로 만들까?
+//포인터 때문이 확실.. 구조체는 복사되니까.
 
 class Node {
-    var data: Int
+    var data: Double
     var next: Node?
     
-    init(data: Int, next: Node? = nil) {
+    init(data: Double, next: Node? = nil) {
         self.data = data
         self.next = next
     }
@@ -25,7 +27,7 @@ var head = node1
 
 
 //입력
-func add(data: Int) {
+func add(data: Double) {
     var node: Node? = head
     
     while node!.next != nil {
@@ -36,7 +38,7 @@ func add(data: Int) {
 }
 
 for index in 3...10 {
-    add(data: index)
+    add(data: Double(index))
 }
 
 
@@ -48,6 +50,42 @@ while node!.next != nil {
     node = node!.next
 }
 print(node?.data ?? 0)
+
+
+
+
+//중간에 데이터 추가하기
+
+var node3 = Node(data: 3.5)
+node = head
+var search = true
+
+while search {
+    if node?.data == 3.0 {
+        search = false
+    } else {
+        node = node?.next
+    }
+}
+
+let nonext = node?.next
+node?.next = node3
+node3.next = nonext
+
+
+print("==================================================================")
+
+
+//출력
+node = head
+
+while node!.next != nil {
+    print(node?.data ?? 0)
+    node = node!.next
+}
+print(node?.data ?? 0)
+
+
 
 
 
