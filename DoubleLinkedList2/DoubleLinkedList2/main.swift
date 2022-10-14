00b8b45aa33ee1a7a0fc062eb32f263f6c35f4ba
kickbell <dev.kickbell@gmail.com>
2022-10-14 16:26:00 +0900

    Add 중간에 데이터 추가 기능

--- DoubleLinkedList2/DoubleLinkedList2/main.swift
+++ DoubleLinkedList2/DoubleLinkedList2/main.swift
@@ -20,16 +20,16 @@ class Node<T> {
     }
 }
 
-class NodeManager<Int: Equatable> {
-    var head: Node<Int>?
-    var tail: Node<Int>?
-    var node: Node<Int>?
+class NodeManager<Double: Equatable> {
+    var head: Node<Double>?
+    var tail: Node<Double>?
+    var node: Node<Double>?
     
-    init(data: Int) {
+    init(data: Double) {
         self.head = Node(data: data)
     }
     
-    func add(_ data: Int) {
+    func add(_ data: Double) {
         //1. 포인터를 타고 넘어갈 시작부분 할당
         self.node = self.head
         //2. 마지막 노드를 찾아감. while문을 통과하면 node는 데이터는 있고 다음 next는 nil인 마지막 노드를 가리키게 됨.
@@ -45,26 +45,40 @@ class NodeManager<Int: Equatable> {
         self.tail = newNode
     }
     
-    //    func delete(_ data: Int) {
-    //        //1. 포인터를 타고 넘어갈 시작부분 할당
-    //        self.node = self.head
-    //
-    //        while self.node?.next != nil {
-    //            //2. 삭제하고자 하는 노드 찾기
-    //            if let unwrapData = self.node?.next?.data, unwrapData == data {
-    //                self.node?.next = self.node?.next?.next
-    //                return
-    //            }
-    //            self.node = self.node?.next
-    //        }
-    //    }
-    
+    //연습3: 위 코드에서 노드 데이터가 특정 숫자인 노드 앞에 데이터를 추가하는 함수를 만들고, 테스트해보기</font></strong><br>
+    //- 더블 링크드 리스트의 tail 에서부터 뒤로 이동하며, 특정 숫자인 노드를 찾는 방식으로 함수를 구현하기<br>
+    //- 테스트: 임의로 0 ~ 9까지 데이터를 링크드 리스트에 넣어보고, 데이터 값이 2인 노드 앞에 1.5 데이터 값을 가진 노드를 추가해보기
+    func insert(_ data: Double, beforeData: Double) {
+        self.node = self.tail //1. 뒤에서 부터 갈거니까 tail 할당
+        
+        //2. beforeData앞에 넣을거니까 그걸 먼저 찾는다. 다르다면 한칸 앞으로 이동
+        while self.node!.data != beforeData {
+            self.node = self.node?.prev
+            if node?.prev == nil { //맨 앞까지 왔다면 해당 데이터가 없는거니까 그냥 break
+                print("해당 beforeData : \(beforeData) 는 존재하지 않습니다.")
+                return
+            }
+        }
+        
+        //3. 여기까지 왔다면 self.node == beforeData를 갖는 노드라는 말이야. 이 애 앞에 새로운 데이터를 넣을거야.
+        let preNode = node?.prev
+        let newNode = Node(data: data)
+        
+        //4. 새로만든 노드의 next 포인터 수정작업
+        preNode?.next = newNode
+        newNode.prev = preNode
+        
+        //5. 새로만든 노드의 prev 포인터 수정작업
+        newNode.next = self.node
+        self.node?.prev = newNode
+    }
+        
     enum Direction {
         case forward
         case backward
     }
     
-    func search(_ direction: Direction, _ data: Int) {
+    func search(_ direction: Direction, _ data: Double) {
         switch direction {
         case .forward:
             self.node = self.head
@@ -111,16 +125,17 @@ class NodeManager<Int: Equatable> {
 }
 
 
-let linkedList = NodeManager(data: 0)
+let linkedList = NodeManager(data: 0.0)
 
 print("\n==추가==")
-(1...10).forEach { linkedList.add($0) }
+(1...10).map { Double($0)}
+    .forEach{ linkedList.add($0) }
 linkedList.desc()
 
 print("\n==검색==")
 linkedList.search(.forward, 5)
 linkedList.search(.backward, 8)
 
-//print("\n==삭제==")
-//linkedList.delete(3)
-//linkedList.desc()
+print("\n==중간에 데이터 추가==")
+linkedList.insert(5.5, beforeData: 6)
+linkedList.desc()
