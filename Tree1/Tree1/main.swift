a56aa8500b75abf1fca4a04b78614330bb22e014
kickbell <dev.kickbell@gmail.com>
2022-10-17 16:19:29 +0900

    Add search

--- Tree1/Tree1/main.swift
+++ Tree1/Tree1/main.swift
@@ -24,9 +24,9 @@ class NodeMgmt {
         self.rootNode = rootNode
     }
     
-    func insert(value: Int) {
+    func insert(value: Int) -> Int {
         //루트노드를 생성하고 현재 노드로 할당
-        var currentNode = self.rootNode
+        var currentNode: Node = self.rootNode
         
         while true {
             if value < currentNode.value {
@@ -35,7 +35,7 @@ class NodeMgmt {
                     //currentNode.left가 nil이면 더이상 데이터가 없는것이니 새로운 노드를 만들어서 할당하면 끝
                     //while문을 빠져나가야 하니 break 필수
                     currentNode.left = Node(value: value)
-                    break
+                    return value
                 } else {
                     //currentNode.left가 nil이 아니라면 데이터가 더 있다는거니까 기준이 되는 노드를 바꿔줘서 다시 비교를 해야함.
                     currentNode = currentNode.left!
@@ -44,19 +44,48 @@ class NodeMgmt {
                 //새로 들어온 데이터가 현재 노드보다 크다면 오른쪽
                 if currentNode.right == nil {
                     currentNode.right = Node(value: value)
-                    break
+                    return value
                 } else {
                     currentNode = currentNode.right!
                 }
             }
         }
     }
+    
+    
+    func search(value: Int) -> Bool {
+        //맨 위부터 내려가야되니까 초기값을 할당해준건데, 위와는 다르게 옵셔널로 해준이유는 while문을 끝까지 돌게 하려고 한거야.
+        //currentNode 가 nil이라는 말은
+        var currentNode: Node? = self.rootNode
+        
+        while currentNode != nil {
+            guard let cNode = currentNode else { break }
+            if cNode.value == value {
+                return true
+            } else if value < cNode.value {
+                currentNode = cNode.left
+            } else {
+                currentNode = cNode.right
+            }
+        }
+        return false
+    }
 }
 
 
-let rootNode = Node(value: 1)
+let rootNode = Node(value: 13)
 let bst = NodeMgmt(rootNode: rootNode)
-bst.insert(value: 2)
+
+print("\n=== insert ===")
+print(bst.insert(value: 21))
+print(bst.insert(value: 15))
+print(bst.insert(value: 25))
+print(bst.insert(value: 42))
+print(bst.insert(value: 33))
+
+
+print("\n=== search ===")
+print(bst.search(value: 25))
 
 
 
