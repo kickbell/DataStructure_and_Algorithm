57a0a52b66c352b721d06e2623fb4ad220705057
kickbell <dev.kickbell@gmail.com>
2022-10-20 23:50:44 +0900

    burble sort 3

--- Sort/Sort/main.swift
+++ Sort/Sort/main.swift
@@ -11,7 +11,7 @@ func processTime(closure: () -> ()){
     let start = CFAbsoluteTimeGetCurrent()
     closure()
     let processTime = CFAbsoluteTimeGetCurrent() - start
-    print("경과 시간: \(processTime)")
+    print("경과 시간: \(processTime)\n")
 }
 
 
@@ -90,6 +90,35 @@ func bubblesort2(_ array: [Int]) -> [Int] {
     return result
 }
 
+
+
+/*
+ 코드 최적화작업 2
+ 
+ 추가적으로 최적화 할 수 있는 작업이 있다. 일종의 예외처리 코드일 수 있겠다.
+ 예를 들어 [1, 2, 3, 5] 라는 데이터가 있다고 치자.
+ 근데 얘는 이미 정렬이 되어있다. 그렇다면 바로 종료해야 하지 않을까? 그럼 그걸 어떻게 체크할까?
+ swap이 false라는 것은 이미 정렬이 다 되어있다는 것이고 true는 그렇지 않다는 것이다.
+ 코드에 isSwap 이라는 Bool 타입 변수를 하나 만든다. 그다음에 for문을 한번 돈다.
+ 그런데 정렬된 코드라면? 1cycle만 돌고 반복문을 종료시켜버리는 것이다.
+ */
+func bubblesort3(_ array: [Int]) -> [Int] {
+    var result = array
+    for index1 in 0..<array.count - 1 {
+        var isSwap = false //swap 위치가 중요하다.  1번 스왑되면 바로 정렬이 될 수 있으니까.
+        for index2 in 0..<array.count - 1 - index1 { //결국엔 여기다.
+            //첫번째 0, 두번째 1, 세번째 2 하면 딱 맞다.
+            if result[index2] > result[index2 + 1] {
+                result.swapAt(index2, index2 + 1)
+                isSwap = true
+            }
+        }
+        if isSwap == false { break }
+    }
+    
+    return result
+}
+
 var list: Set<Int> = []
 
 while list.count < 50 {
@@ -97,11 +126,28 @@ while list.count < 50 {
     list.insert(randomInt)
 }
 
-print("===before bubble sort2===")
+
+
+//1. bubblesort1 와 bubblesort2 비교
+print("\n===before bubble sort===")
 print(list)
 
-print("===after bubble sort2===")
+print("\n===after bubble sort===")
 processTime { print(bubblesort1(Array(list))) }
 processTime { print(bubblesort2(Array(list))) }
+processTime { print(bubblesort3(Array(list))) }
+
+
+//2. bubblesort2 와 bubblesort3 비교
+//실제로 위에서는 2,3 이 딱히 차이나지 않거나 비슷하다. 하지만 일부 정렬된 데이터를 넣어버리면 차이는 크다.
+print("\n===after bubble sort===")
+let sortedList = [1, 2, 3, 33, 5, 6, 8, 9, 85, 13, 14, 15, 16, 20, 88, 27, 31, 33, 34, 35, 37, 38, 43, 46, 47, 51, 52, 53, 54, 62, 63, 64, 65, 70, 73, 75, 77, 78, 81, 87, 89, 90, 93, 94, 96, 98, 100]
+processTime { print(bubblesort2(sortedList)) }
+processTime { print(bubblesort3(sortedList)) }
+
+
+
+
+
 
 
