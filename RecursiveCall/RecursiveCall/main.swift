effdb2005813278d7262996100e64ba26df56685
kickbell <dev.kickbell@gmail.com>
2022-10-21 17:01:01 +0900

    add calculation

--- RecursiveCall/RecursiveCall/main.swift
+++ RecursiveCall/RecursiveCall/main.swift
@@ -125,3 +125,68 @@ print(palindrome2("madem"))
 print(palindrome2("kayak"))
 print(palindrome2("kaydk"))
 
+
+/*
+ 프로그래밍 연습
+ 1, 정수 n에 대해
+ 2. n이 홀수이면 3 X n + 1 을 하고,
+ 3. n이 짝수이면 n 을 2로 나눕니다.
+ 4. 이렇게 계속 진행해서 n 이 결국 1이 될 때까지 2와 3의 과정을 반복합니다.
+
+ 예를 들어 n에 3을 넣으면,
+ 3
+ 10
+ 5
+ 16
+ 8
+ 4
+ 2
+ 1
+ 이 됩니다.
+ 이렇게 정수 n을 입력받아, 위 알고리즘에 의해 1이 되는 과정을 모두 출력하는 함수를 작성하세요.
+ */
+
+func calculation1(_ num: Int) {
+    print("n: \(num)")
+    
+    var n = num
+    while n != 1 {
+        if n % 2 == 0 {
+            n = n / 2
+        } else {
+            n = 3 * n + 1
+        }
+        print("n: \(n)")
+    }
+}
+
+func calculation2(_ num: Int) {
+    print("n: \(num)")
+    //대부분은 혹시 놓칠 케이스 때문에 <= 1 이렇게 하는데, 여기서는 문제에서 1 이라고 명시가 되어있어서 그냥 1로.
+    if num == 1 { return }
+    calculation2((num % 2 == 0) ? (num / 2) : (3 * num) + 1)
+}
+
+print("\n===calculation1===")
+calculation1(3)
+print("\n===calculation2===")
+calculation2(3)
+
+
+
+
+/*
+ 프로그래밍 연습
+ 문제: 정수 4를 1, 2, 3의 조합으로 나타내는 방법은 다음과 같이 총 7가지가 있음
+ 1+1+1+1
+ 1+1+2
+ 1+2+1
+ 2+1+1
+ 2+2
+ 1+3
+ 3+1
+ 정수 n이 입력으로 주어졌을 때, n을 1, 2, 3의 합으로 나타낼 수 있는 방법의 수를 구하시오
+ 힌트: 정수 n을 만들 수 있는 경우의 수를 리턴하는 함수를 f(n) 이라고 하면,
+ f(n)은 f(n-1) + f(n-2) + f(n-3) 과 동일하다는 패턴 찾기
+ 출처: ACM-ICPC > Regionals > Asia > Korea > Asia Regional - Taejon 2001
+ */
