41f58ffa607df5c1a9ca74152295aaabf6b4c4be
kickbell <dev.kickbell@gmail.com>
2022-10-30 07:29:13 +0900

    prim 2

--- Prim/Prim/main.swift
+++ Prim/Prim/main.swift
@@ -82,7 +82,7 @@ func prim(_ startNode: String, _ edges: [Edge]) -> [Edge] {
     return mst
 }
 
-print("\n===prim===")
+print("\n===prim=== ")
 prim("A", edges).forEach { print($0) }
 
 /*
@@ -97,3 +97,45 @@ prim("A", edges).forEach { print($0) }
   (5, 'E', 'C'),
   (9, 'E', 'G')]
  */
+
+
+/*
+ 개선된 프림 알고리즘
+ - 간선이 아니라 노드를 기준으로 하기 때문에 시간복잡도가 𝑂(𝐸𝑙𝑜𝑔𝑉) 로 줄어든다.
+ 
+ from heapdict import heapdict
+
+ def prim(graph, start):
+     mst, keys, pi, total_weight = list(), heapdict(), dict(), 0
+     for node in graph.keys():
+         keys[node] = float('inf')
+         pi[node] = None
+     keys[start], pi[start] = 0, start
+
+     while keys:
+         current_node, current_key = keys.popitem()
+         mst.append([pi[current_node], current_node, current_key])
+         total_weight += current_key
+         for adjacent, weight in mygraph[current_node].items():
+             if adjacent in keys and weight < keys[adjacent]:
+                 keys[adjacent] = weight
+                 pi[adjacent] = current_node
+     return mst, total_weight
+ 
+ 
+ 
+ mygraph = {
+     'A': {'B': 7, 'D': 5},
+     'B': {'A': 7, 'D': 9, 'C': 8, 'E': 7},
+     'C': {'B': 8, 'E': 5},
+     'D': {'A': 5, 'B': 9, 'E': 7, 'F': 6},
+     'E': {'B': 7, 'C': 5, 'D': 7, 'F': 8, 'G': 9},
+     'F': {'D': 6, 'E': 8, 'G': 11},
+     'G': {'E': 9, 'F': 11}
+ }
+ mst, total_weight = prim(mygraph, 'A')
+ print ('MST:', mst)
+ print ('Total Weight:', total_weight)
+ */
+
+
